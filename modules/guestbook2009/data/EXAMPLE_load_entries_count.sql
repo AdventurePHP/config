0ede9ad7e836f0149b4e8976fcb33fafbac0667f
Christian Achatz <christian.achatz@adventure-php-framework.org>
2009-11-30 18:04:38 +0000

    Corrected pager statement configuration files for the guestbook2009 module.

--- modules/guestbook2009/data/EXAMPLE_load_entries_count.sql
+++ modules/guestbook2009/data/EXAMPLE_load_entries_count.sql
@@ -1,5 +1,4 @@
 SELECT COUNT(ent_entry.EntryID) AS EntriesCount FROM ent_entry
 INNER JOIN cmp_guestbook2entry ON ent_entry.EntryID = cmp_guestbook2entry.EntryID
 INNER JOIN ent_guestbook ON cmp_guestbook2entry.GuestbookID = ent_guestbook.GuestbookID
-WHERE ent_guestbook.GuestbookID = '[GuestbookID]'
-LIMIT [Start],[EntriesCount];
+WHERE ent_guestbook.GuestbookID = '[GuestbookID]';
