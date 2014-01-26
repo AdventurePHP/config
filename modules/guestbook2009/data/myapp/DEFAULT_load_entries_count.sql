a114f441570c14c2f5dee49d1a6001df6eab4ba0
Christian Achatz <christian.achatz@adventure-php-framework.org>
2014-01-26 15:57:40 +0000

    ID#26: corrected config location.

--- modules/guestbook2009/data/myapp/DEFAULT_load_entries_count.sql
+++
@@ -1,4 +0,0 @@
-SELECT COUNT(ent_entry.EntryID) AS EntriesCount FROM ent_entry
-INNER JOIN cmp_guestbook2entry ON ent_entry.EntryID = cmp_guestbook2entry.Target_EntryID
-INNER JOIN ent_guestbook ON cmp_guestbook2entry.Source_GuestbookID = ent_guestbook.GuestbookID
-WHERE ent_guestbook.GuestbookID = '[GuestbookID]';
